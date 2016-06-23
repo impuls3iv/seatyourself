570868e3c8a8e4a49a80b354268c0a3166b11de6
Impuls3iv <sammy.unterman@gmail.com>
2016-06-23 13:44:44 -0400

    completed restaurants controller CRUD actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,48 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
-  end
-
-  def delete
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def show
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_url(@restaurant)
+    else
+      render :new
+    end
   end
+
+  def delete
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_url(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :price, :capacity)
+  end
+
 end
