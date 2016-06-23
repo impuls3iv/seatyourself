3aae4f4036e5a2f780eec22ee28a44c7621eae8c
Rigil-Kent <tsubashi.gurung@gmail.com>
2016-06-23 15:46:48 -0400

    created show action, fixed form partial and updated display and view

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,6 +17,10 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
   def edit
     @reservation = Reservation.find(params[:id])
   end
