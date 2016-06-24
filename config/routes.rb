ae0f165982c52e2ca05027a149d2b531e15d1614
Impuls3iv <sammy.unterman@gmail.com>
2016-06-24 12:05:51 -0400

    fixed comma error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :restaurants do
-    resources :reservations, only: [:show, , :new, :create, :destroy]
+    resources :reservations, only: [:show, :new, :create, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
